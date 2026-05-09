dbcbb7a71e4f59ca7e12b3d84998cf1add4136e3
zljcode <1393300842@qq.com>
2026-05-09 23:19:00 +0800

    feat: 协议处理案例全链路打通

--- ai-mcp-gateway-domain/src/main/java/cn/zephyr/ai/domain/session/service/message/SessionMessageService.java
+++ ai-mcp-gateway-domain/src/main/java/cn/zephyr/ai/domain/session/service/message/SessionMessageService.java
@@ -5,6 +5,7 @@ import cn.zephyr.ai.domain.session.model.valobj.enums.SessionMessageHandlerMetho
 import cn.zephyr.ai.domain.session.service.ISessionMessageService;
 import cn.zephyr.ai.domain.session.service.message.handler.IRequestHandler;
 import cn.zephyr.ai.types.exception.AppException;
+import com.alibaba.fastjson.JSON;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,22 +27,35 @@ public class SessionMessageService implements ISessionMessageService {
     private Map<String, IRequestHandler> requestHandlerMap;
 
     @Override
-    public McpSchemaVO.JSONRPCResponse processHandlerMessage(McpSchemaVO.JSONRPCRequest request) {
-        String method = request.method();
-        log.info("开始处理方法，方法：{}",method);
+    public McpSchemaVO.JSONRPCResponse processHandlerMessage(McpSchemaVO.JSONRPCMessage message) {
 
-        SessionMessageHandlerMethodEnum sessionMessageHandlerMethodEnum = SessionMessageHandlerMethodEnum.getByMethod(method);
-        if(null == sessionMessageHandlerMethodEnum){
-            throw new AppException(METHOD_NOT_FOUND.getCode(), METHOD_NOT_FOUND.getInfo());
+        if (message instanceof McpSchemaVO.JSONRPCResponse response) {
+            log.info("收到结果消息");
         }
 
-        String handlerName = sessionMessageHandlerMethodEnum.getHandlerName();
-        IRequestHandler requestHandler = requestHandlerMap.get(handlerName);
-        if(null == requestHandler){
-            throw new AppException(METHOD_NOT_FOUND.getCode(), METHOD_NOT_FOUND.getInfo());
+        if (message instanceof McpSchemaVO.JSONRPCRequest request) {
+            String method = request.method();
+            log.info("开始处理请求，方法：{}", method);
+
+            SessionMessageHandlerMethodEnum sessionMessageHandlerMethodEnum = SessionMessageHandlerMethodEnum.getByMethod(method);
+            if (null == sessionMessageHandlerMethodEnum) {
+                throw new AppException(METHOD_NOT_FOUND.getCode(), METHOD_NOT_FOUND.getInfo());
+            }
+
+            String handlerName = sessionMessageHandlerMethodEnum.getHandlerName();
+            IRequestHandler requestHandler = requestHandlerMap.get(handlerName);
+
+            if (null == requestHandler) {
+                throw new AppException(METHOD_NOT_FOUND.getCode(), METHOD_NOT_FOUND.getInfo());
+            }
+
+            //使用枚举策略模式处理请求
+            return requestHandler.handle(request);
         }
 
-        //使用枚举策略模式处理请求
-        return requestHandler.handle(request);
+        if (message instanceof McpSchemaVO.JSONRPCNotification notification) {
+            log.info("收到即将处理的通知 {} {}", notification.method(), JSON.toJSONString(notification.params()));
+        }
+        return null;
     }
 }
